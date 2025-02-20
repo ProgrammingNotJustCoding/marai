265274c90e670a364c9863c8b9fdb4e5c807b5c7
Inigojeevan <in1442@srmist.edu.in>
2025-02-20 20:55:50 +0530

    feat:Base homepage

---
+++ client/src/components/common/Layout.tsx
@@ -0,0 +1,15 @@
+const Layout = () => {
+    return (
+        <div>
+            <nav>
+            <ul>
+                <li><a href="/about">About</a></li>
+                <li><a href="/sign_in">Sign In</a></li>
+            </ul>
+            </nav>
+            <h1>Layout</h1>
+        </div>
+    )
+}
+
+export default Layout;
---
+++ client/src/components/home/Home.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <main className="w-screen h-screen flex flex-col">
+      <h1>
+        Home
+      </h1>
+    </main>
+  );
+}
+
+export default Home;
